f459029ea16e74abdf286e5e08b93468989dd2f2
BoarderGamez <115997081+BoarderGamez@users.noreply.github.com>
2025-08-08 13:18:45 +0000

    the newest commit

--- main.py
+++ main.py
@@ -1,5 +1,20 @@
 from tkinter import *
+import socket
 
+def check_connection():
+    try:
+        # Try connecting to Google DNS
+        socket.create_connection(("8.8.8.8", 53))
+        return True
+    except OSError:
+        return False
+
+root = Tk()
+
+if check_connection():
+    Label(root, text="Connected to tYour om anahajgoodcoding sckills me dcoing coding aaah coding he Internet").pack()
+else:
+    Label(root, text="No Internet Connection").pack()
 # Init
 root = Tk()
 
